1c161b1d9a702073526870e5af1887a2d6c18e73
Emily Robinson <robinson.es@gmail.com>
2016-06-01 16:36:46 +0200

    updated answers

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,1 +1,21 @@
-PLACE YOUR CODE HERE
+#### Q1
+
+# import faculty file
+import pandas as pd
+import numpy
+# read file as dataframe
+faculty = pd.read_csv('/users/emilyrobinson/Dropbox/metis/prework/dsp/python/faculty.csv')
+faculty
+# column "degree" has all degrees. Sometimes there are different notations for same degree (e.g. Ph.D. versus PhD). Some people have multiple degrees
+# make a list of all degrees
+degrees = faculty[' degree'].tolist()
+# remove "." to fix formatting  
+degrees_nodots = [i.replace(".", "") for i in degrees]
+# split strings where a space (to separate multiple degrees) and make one list again
+degrees_split = [i.split() for i in degrees_nodots]
+degrees_onelist = [item for sublist in degrees_split for item in sublist] 
+# Count occurances of each type of degree
+from collections import Counter
+Counter(degrees_onelist)
+
+
--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -4,8 +4,6 @@
 # against opponents, and had 36 goals scored against them). Write a program to read the file, 
 # then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
 
-import csv
-
 import pandas as pd
 
 # read file as dataframe
