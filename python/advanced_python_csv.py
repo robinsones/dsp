7ec562ee85d64f6cbedbc93322a85a177b7d3bb6
robinsones <robinson.es@gmail.com>
2016-06-01 17:18:29 +0200

    Answered Emails

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,16 @@
-PLACE YOUR CODE HERE
+# Create csv file from emails
+import pandas as pd
+import numpy
+# read file as dataframe
+faculty = pd.read_csv('/users/emilyrobinson/Dropbox/metis/prework/dsp/python/faculty.csv')
+
+# get emails
+emails = faculty[' email'].tolist()
+
+# create csv file from emails
+
+import csv
+resultFile = open("emails.csv",'wb')
+wr = csv.writer(resultFile, dialect='excel')
+for item in emails:
+     wr.writerow([item, ])
